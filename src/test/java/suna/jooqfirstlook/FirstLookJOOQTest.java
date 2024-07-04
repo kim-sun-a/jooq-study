013586a4348759e4e209b129be3534d32be6b160
sakim <gina305496@gmail.com>
2024-07-04 16:05:11 +0900

    jooq custom 세팅 및 config 설정 추가, test 추가

---
+++ src/test/java/suna/jooqfirstlook/FirstLookJOOQTest.java
@@ -0,0 +1,21 @@
+package suna.jooqfirstlook;
+
+import org.jooq.DSLContext;
+import org.jooq.generated.tables.JActor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FirstLookJOOQTest {
+
+    @Autowired
+    DSLContext dslContext;
+
+    @Test
+    void test() {
+        dslContext.selectFrom(JActor.ACTOR)
+                .limit(10)
+                .fetch();
+    }
+}
