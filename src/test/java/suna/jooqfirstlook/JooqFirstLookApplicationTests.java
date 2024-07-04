42a1afcf4c5bc42cc8b9bd9ac0d912c9a876beb5
sakim <gina305496@gmail.com>
2024-07-04 14:58:15 +0900

    init

---
+++ src/test/java/suna/jooqfirstlook/JooqFirstLookApplicationTests.java
@@ -0,0 +1,13 @@
+package suna.jooqfirstlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqFirstLookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
